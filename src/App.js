31a13c5195c4d5d615a17d2502430d7b68d6b669
Nupur Tevatiya <nupurnupurtevatiya@gmail.com>
2021-12-29 13:25:42 +0530

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
-import SignInPage from "./pages/signin";
+import { Route, Routes,} from "react-router-dom";
 import SignUpPage from "./pages/signup";
 import HomePage from "./pages/home";
 import ProfilePage from "./pages/profile";
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" exact element={<HomePage />} />
-        <Route path="/signin" exact element={<SignInPage />} />
         <Route path="/signup" exact element={<SignUpPage />} />
-        <Route path="/" element={<Navigate replace to="/signin" />} />
         <Route path="/profile" exact element={<ProfilePage/>} />
       </Routes>
     </div>
